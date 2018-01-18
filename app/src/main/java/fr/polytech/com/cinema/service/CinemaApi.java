f215b07a5667ab4514f8ddfea4295cb07767eb82
Grégoire Piat <gregoire.piat@etu.univ-lyon1.fr>
2018-01-18 12:24:51 +0100

    Add project generated files

---
+++ app/src/main/java/fr/polytech/com/cinema/service/CinemaApi.java
@@ -0,0 +1,29 @@
+package fr.polytech.com.cinema.service;
+
+/**
+ * @author GregoirePiat - gregoire.piat.dev@gmail.com
+ */
+
+import java.util.List;
+
+import fr.polytech.com.cinema.entity.Actor;
+import fr.polytech.com.cinema.entity.Movie;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface CinemaApi {
+
+    @GET("peoples/")
+    Call<List<Actor>> getActors();
+
+    @GET("people/{id}")
+    Call<Actor> getActorById(@Path("people") String actorId);
+
+    @GET("movies/")
+    Call<List<Movie>> getMovies();
+
+    @GET("movie/{id}")
+    Call<Movie> getMovieById(@Path("movie") String actorId);
+}
